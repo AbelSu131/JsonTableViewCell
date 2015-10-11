75cbd45bf05054a9f23d313f2ccbdaa5a99fd5d9
AbelSu <abel@abeldemacbook-pro.local>
2015-10-12 07:44:21 +0800

    Initial Commit

---
+++ JsonTableViewCell/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JsonTableViewCell
+//
+//  Created by abel on 15/10/12.
+//  Copyright © 2015年 abel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ JsonTableViewCell/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JsonTableViewCell
+//
+//  Created by abel on 15/10/12.
+//  Copyright © 2015年 abel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
